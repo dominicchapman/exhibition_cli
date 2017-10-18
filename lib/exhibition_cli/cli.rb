8bd1cc20c68fb552adcd467212371f349b453af6
Dominic Chapman <dominicchapman@me.com>
2017-10-18 21:36:56 +0100

    Add dummy exhibition list and menu to cli controller

--- lib/exhibition_cli/cli.rb
+++ lib/exhibition_cli/cli.rb
@@ -4,6 +4,7 @@ class ExhibitionCli::Cli
   def call
     list_exhibitions
     menu
+    adios
   end
 
   def list_exhibitions
@@ -16,7 +17,25 @@ class ExhibitionCli::Cli
   end
 
   def menu
-    puts "Which exhibition would you like more information about?"
+    choice = nil
+    while choice != 'exit'
+      puts "Learn more about an exhibition by typing its number, list all exhibitions with list, or type exit to finish."
+      choice = gets.strip.downcase
+      case choice
+      when "1"
+        puts "More info on Items: Is Fa..."
+      when "2"
+        puts "More info on Louise Bourg..."
+      when "list"
+        list_exhibitions
+      else
+        "Hmm, can't find that. Type list or exit."
+      end
+    end
+  end
+
+  def adios
+    puts "Every exit is also an entrance... Goodbye, for now."
   end
 
 end
